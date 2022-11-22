187f5295c303152172d4592b0e1507c243d42dc0
Maja <95423761+mayamalkiewicz@users.noreply.github.com>
2022-11-22 19:30:54 +0100

    Changes in js.js

--- js.js
+++ js.js
@@ -3,14 +3,12 @@ function computerRandomChoice(x) {
     return Math.floor(Math.random() * x);
   }
   
-  //console.log(computerRandomChoice(3));
-  // expected output: 0, 1 or 2
-  // works
+//console.log(computerRandomChoice(3));
+// expected output: 0, 1 or 2
 
-  // 1. ...the numerical output 0,1,2.
-  let computerWeaponAssignment = computerRandomChoice(3)
-  // console.log(computerWeaponAssignment);
-  //works
+// 1. ...the numerical output 0,1,2.
+let computerWeaponAssignment = computerRandomChoice(3)
+// console.log(computerWeaponAssignment);
 
 
 //2. Assigning to the variable the output of computer choice to the tree particular types of weapon: rock, paper or scissors.
@@ -38,12 +36,11 @@ let computer = assignment();
   }
    
 
-  let playerWeaponChoice = playerChoice("rock"); // <--write your choice in place "weapon" (paper, scissors or rock)
+let playerWeaponChoice = playerChoice("rock"); // <--write your choice in place "weapon" (paper, scissors or rock)
 
-  let player = playerWeaponChoice;
+let player = playerWeaponChoice;
   
 //4. Function checks cases if the player is the winner or the computer is the winner or if it is the tie.
-
 function thePlay(computer, player) {
     console.log(`The computer choice is: ${assignment()}`);
     console.log(`You have chosen ${player}`);
